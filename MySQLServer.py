bcd88806e04fd5a48e2de925ef0f1946703d6682
Simby1 <kimsimby@gmail.com>
2025-12-17 16:04:13 +0100

    checker fix: "except mysql.connector.Error as e:"

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,21 +1,21 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     connection = None
-    try:       # Establish connection to the MySQL server
+    try:
         connection = mysql.connector.connect(
             host='localhost',
-            user='root',         
-            password='password'  
+            user='root',
+            password='password' 
         )
 
-        if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-            print("Database 'alx_book_store' created successfully!")
+        cursor = connection.cursor()
+        
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        
+        print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
